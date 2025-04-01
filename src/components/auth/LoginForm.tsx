8aec63cfb16d068e8dc3c082c17fb563c2a23f7c
Syntrice <syntrice@protonmail.com>
2025-04-01 21:08:56 +0100

    feat: Implement login functionality

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,11 +1,41 @@
-import login from "@/server/actions/login"
+"use client"
+import { signIn } from "next-auth/react"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { useActionState } from "react"
+
+type LoginFormState = string | undefined
 
 export default function LoginForm() {
+  const router = useRouter()
+
+  const login = async (formState: LoginFormState, formData: FormData) => {
+    const username = formData.get("username") as string
+    const password = formData.get("password") as string
+
+    const res = await signIn("credentials", {
+      username: username,
+      password: password,
+      redirect: false,
+    })
+
+    if (res?.error) {
+      return res.error as string
+    }
+    if (res?.ok) {
+      router.push("/")
+      return
+    }
+  }
+
+  const [formState, formAction] = useActionState(login, undefined)
+
+  // login is done client side, so this action goes in this form component
+
   return (
     <div className="max-w-sm grow">
       <h2 className="text-2xl mb-5 text-center">Login</h2>
-      <form className="flex flex-col" action={login}>
+      <form className="flex flex-col" action={formAction}>
         <label className="mb-2" htmlFor="username">
           Username
         </label>
@@ -33,6 +63,7 @@ export default function LoginForm() {
         <Link className="form-button" href="register">
           Register
         </Link>
+        {formState && <p className="my-5 text-red-600">{formState}</p>}
       </form>
     </div>
   )
