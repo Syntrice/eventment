f690fdd4d13b864ba40a94f91d58c773c8bc1c5a
Syntrice <syntrice@protonmail.com>
2025-04-01 13:06:53 +0100

    feat: Create User model
    Setup a mongoose user model with username and password properties. See https://www.mongodb.com/developer/languages/typescript/nextauthjs-authentication-mongodb/ for reference.

---
+++ src/server/models/User.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema, model } from "mongoose"
+
+export interface UserDocument {
+  _id: string
+  username: string
+  password: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+const UserSchema = new Schema<UserDocument>(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+const User = mongoose.models?.User || model<UserDocument>("User", UserSchema)
+
+export default User
