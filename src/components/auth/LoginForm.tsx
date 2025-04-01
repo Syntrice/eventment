c0a0038e77cc1be5450db83cc9c123d75088cfe9
Syntrice <syntrice@protonmail.com>
2025-04-01 21:34:11 +0100

    feat: Remove next-auth
    Remove next-auth and related code. It will be replaced either with
    custom authentication or reimplemented from scratch on latest version.

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,41 +1,13 @@
 "use client"
-import { signIn } from "next-auth/react"
 import Link from "next/link"
-import { useRouter } from "next/navigation"
-import { useActionState } from "react"
-
-type LoginFormState = string | undefined
 
 export default function LoginForm() {
-  const router = useRouter()
-
-  const login = async (formState: LoginFormState, formData: FormData) => {
-    const username = formData.get("username") as string
-    const password = formData.get("password") as string
-
-    const res = await signIn("credentials", {
-      username: username,
-      password: password,
-      redirect: false,
-    })
-
-    if (res?.error) {
-      return res.error as string
-    }
-    if (res?.ok) {
-      router.push("/")
-      return
-    }
-  }
-
-  const [formState, formAction] = useActionState(login, undefined)
-
   // login is done client side, so this action goes in this form component
 
   return (
     <div className="max-w-sm grow">
       <h2 className="text-2xl mb-5 text-center">Login</h2>
-      <form className="flex flex-col" action={formAction}>
+      <form className="flex flex-col">
         <label className="mb-2" htmlFor="username">
           Username
         </label>
@@ -63,7 +35,6 @@ export default function LoginForm() {
         <Link className="form-button" href="register">
           Register
         </Link>
-        {formState && <p className="my-5 text-red-600">{formState}</p>}
       </form>
     </div>
   )
