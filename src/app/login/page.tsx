6aa4d8b5eb2f734d804c98fa65b4fd15ec46fb45
Syntrice <syntrice@protonmail.com>
2025-04-02 15:26:00 +0100

    feat: Use form for github login button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,13 @@
+import GitHubLoginForm from "@/components/auth/GitHubLoginForm"
 import LoginForm from "@/components/auth/LoginForm"
-import GitHubIcon from "@/components/icons/GitHubIcon"
 import Link from "next/link"
 
 export default function Login() {
   return (
     <main className="p-5 flex justify-center py-20 max-md:py-10">
       <div className="max-w-sm grow text-center">
-        <h2 className="text-2xl mb-5 text-center">Login</h2>
-        <button className="form-button w-full flex justify-center items-center gap-1">
-          <GitHubIcon className="inline text-xl" />
-          <span>Sign in with GitHub</span>
-        </button>
+        <h2 className="text-2xl mb-10 text-center">Login</h2>
+        <GitHubLoginForm />
         <p className="my-5">Or continue with email</p>
         <LoginForm />
         <p className="my-5">Don&apos;t have an account with us yet?</p>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,16 +1,13 @@
+import GitHubLoginForm from "@/components/auth/GitHubLoginForm"
 import RegisterForm from "@/components/auth/RegisterForm"
-import GitHubIcon from "@/components/icons/GitHubIcon"
 import Link from "next/link"
 
 export default function Register() {
   return (
     <main className="p-5 flex justify-center py-20 max-md:py-10">
       <div className="max-w-sm grow text-center">
-        <h2 className="text-2xl mb-5 text-center">Register</h2>
-        <button className="form-button w-full flex justify-center items-center gap-1">
-          <GitHubIcon className="inline text-xl" />
-          <span>Sign in with GitHub</span>
-        </button>
+        <h2 className="text-2xl mb-10 text-center">Register</h2>
+        <GitHubLoginForm />
         <p className="my-5">Or continue with email</p>
         <RegisterForm />
         <p className="my-5">Already have an account with us?</p>
