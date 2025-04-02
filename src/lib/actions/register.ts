79bc67081c9a5048370f017f90ba3f204ba3f860
Syntrice <syntrice@protonmail.com>
2025-04-02 20:13:53 +0100

    feat: Add error handling to actions

--- src/lib/actions/register.ts
+++ src/lib/actions/register.ts
@@ -1,11 +1,24 @@
 "use server"
 
+import { isRedirectError } from "next/dist/client/components/redirect-error"
 import ActionResponse from "../definitions/ActionResponse"
 
 export default async function register(): Promise<ActionResponse> {
-  console.log("Signing up...")
-  return {
-    success: false,
-    message: "Credential based registration has not yet been implemented.",
+  try {
+    console.log("Signing up...")
+    return {
+      success: false,
+      message: "Credential based registration has not yet been implemented.",
+    }
+  } catch (error) {
+    if (isRedirectError(error)) {
+      // this error should be handled further up the call stack (as it is may trigger a redirect)
+      throw error
+    }
+
+    return {
+      success: false,
+      message: "An error occurred while registering.",
+    }
   }
 }
