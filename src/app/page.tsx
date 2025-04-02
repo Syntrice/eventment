ba206307f4b77a899e681214065d9c49b1db60c6
Syntrice <syntrice@protonmail.com>
2025-04-02 15:38:00 +0100

    feat: Show authentication on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <main></main>
+import { auth } from "@/lib/auth"
+
+export default async function Home() {
+  const session = await auth()
+
+  return (
+    <main className="p-5">
+      <h1 className="text-2xl">{session ? "Logged in" : "Not logged in"}</h1>
+    </main>
+  )
 }
