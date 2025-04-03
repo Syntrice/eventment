097828331922e954166ff7e4e78adbcda904c53a
Syntrice <syntrice@protonmail.com>
2025-04-04 00:33:01 +0100

    feat: Implement working create event form
    Implement a working create event form, which posts a new event to the database.

--- src/components/events/CreateEventForm.tsx
+++ src/components/events/CreateEventForm.tsx
@@ -1,12 +1,20 @@
 "use client"
 
+import createEvent from "@/lib/actions/create-event"
+import { useActionState } from "react"
+
 export default function CreateEventForm() {
+  const [state, action, pending] = useActionState(createEvent, undefined)
+
   return (
-    <form className="flex flex-col gap-5">
+    <form className="flex flex-col gap-5" action={action}>
       <label className="text-left" htmlFor="title" aria-required={true}>
         Title
       </label>
       <input className="input-text" type="text" name="title" id="" />
+      {state?.errors?.title && (
+        <p className="text-red-400 mb-5 text-left">{state.errors.title}</p>
+      )}
       <label className="text-left" htmlFor="description">
         Description
       </label>
@@ -16,11 +24,25 @@ export default function CreateEventForm() {
         name="description"
         id=""
       ></textarea>
+      {state?.errors?.description && (
+        <p className="text-red-400 mb-5 text-left">
+          {state.errors.description}
+        </p>
+      )}
       <label aria-required={true} className="text-left" htmlFor="date">
         Date
       </label>
       <input className="input-text" type="date" name="date" id="date" />
-      <button className="form-button">Create Event</button>
+      {state?.errors?.date && (
+        <p className="text-red-400 mb-5 text-left">{state.errors.date}</p>
+      )}
+      <button
+        disabled={pending}
+        aria-disabled={pending}
+        className="form-button"
+      >
+        Create Event
+      </button>
     </form>
   )
 }
